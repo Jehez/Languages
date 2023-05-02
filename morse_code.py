5cb60afcb415b6a815122e15f414c91bbfb85be6
Jehez <86358646+Jehez@users.noreply.github.com>
2023-05-02 19:54:30 +0530

    Converted array lookup into hash map
    Array pointer method with linear search was changed to hashmap lookup so O(n^2) converted to O(n)

--- morse_code.py
+++ morse_code.py
@@ -2,17 +2,17 @@
 
 # A common cipher, this program aims to implement it
 # Only letters and numbers will be converted
-# A period '.' will represent a dot, and a hypher '-' a dash
+# A period '.' will represent a dot, and a hyphen '-' a dash
 # a single dash represents break in between letters
 # a triple dash represents break in between words
 
 # Both functions work the same way
-# a pointer moves through an array and stops when the value macthes the desired value
-# the value returned is from the opposite array(same pointer)
+# Previously, an array and pointer was used but worked in O(n^2) - 'n' because of the for loop and one 'n' because of the linear search time
+# Latest implementation uses 2 hashmaps (dicts in python) so lookup is O(1) and total time is O(n)
 
 
-normal = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
-morse = ['.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..','.----','..---','...--','....-','.....','-....','--...','---..','----.','-----']
+normal_hm = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
+morse_hm = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0'}
 
 
 #normal to morse
@@ -20,7 +20,7 @@ def to_morse(sentence):
     new = ''
     for word in sentence.split():
         for letter in word:
-            new+=morse[normal.index(letter)]
+            new+=normal_hm[letter]
             new+=' '
         new+='   '
     return new[:-3]
@@ -29,7 +29,7 @@ def to_morse(sentence):
 def to_normal(code):
     new = ''
     for word in code.split('   '):
-        for letter in word.split():
-            new+=normal[morse.index(letter)]
+        for single_code in word.split():
+            new+=morse_hm[single_code]
         new+=' '
     return new[:-1]
