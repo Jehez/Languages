29ebfbfcc80b0dea9e8f792af5fb9b0714f0001e
Jehez <86358646+Jehez@users.noreply.github.com>
2024-03-15 20:51:07 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -4,14 +4,14 @@ from playsound import playsound
 from os import remove
 
 #a function to make the speech file and play it, default language is english
-def speak(s,l='en'):
-    speech = gTTS(text=s,lang=l,slow=False)     #speech object made
-    speech.save('speech.mp3')                   #speech object saved (as mp3)
-    playsound('speech.mp3')                     #plays the mp3 file
-    remove('speech.mp3')                        #deletes the mp3 file, this is to save space and ensure there is no overwriting on files
+def speak(text,lang='en'):
+    speech = gTTS(text=text,lang=lang,slow=False)    #speech object made
+    speech.save('speech.mp3')                        #speech object saved (as mp3)
+    playsound('speech.mp3')                          #plays the mp3 file
+    remove('speech.mp3')                             #deletes the mp3 file, this is to ensure the file is deleted when no longer in use
+                                                     #also prevents any overwriting/access to files errors is function is called again
 
 #some examples in different languages (all saying 'Hi')
-
 speak('Hi')                 #english
 speak('Salut','fr')         #french
 speak('Hola','es')          #spanish
